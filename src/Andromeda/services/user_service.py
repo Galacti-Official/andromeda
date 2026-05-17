28a9e3f93e2624154f2f00608ae35498fbe7c9a4
Jachym <jachym.vandeneynde@gmail.com>
2026-05-17 17:39:38 +0200

    feat: update user service
    Added a few user service features such as account deletion and user data retrieval.

--- src/Andromeda/services/user_service.py
+++ src/Andromeda/services/user_service.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
 
 from fastapi import HTTPException
+from sqlmodel import select
 from sqlalchemy.exc import IntegrityError
 from sqlmodel.ext.asyncio.session import AsyncSession
 
@@ -12,6 +13,7 @@ from Andromeda.models.user import User
 
 # Schemas 
 from Andromeda.schemas.user import UserCreate, UserPublic
+from Andromeda.schemas.jwt import JWTPayload
 
 
 async def create_user(request: UserCreate, session: AsyncSession) -> UserPublic:
@@ -36,5 +38,28 @@ async def create_user(request: UserCreate, session: AsyncSession) -> UserPublic:
             created_at=user.created_at
         )
     except IntegrityError:
-        raise HTTPException(status_code=409, detail="User with this username or email already exists")
+        raise HTTPException(status_code=409, detail="A user with this username or email already exists")
+    
 
+async def delete_user(user: JWTPayload, session: AsyncSession) -> None:
+    sub_components = user.sub.split(":")
+
+    if len(sub_components) != 2 or sub_components[0] != "user":
+        raise HTTPException(status_code=403, detail="Invalid user type")
+    
+    result = await session.exec(select(User).where(User.id == sub_components[1]))
+    selected_user = result.one_or_none()
+
+    await session.delete(selected_user)
+
+
+async def get_user_data(user: JWTPayload, session: AsyncSession) -> UserPublic:
+    sub_components = user.sub.split(":")
+
+    if len(sub_components) != 2 or sub_components[0] != "user":
+        raise HTTPException(status_code=403, detail="Invalid user type")
+    
+    result = await session.exec(select(User).where(User.id == sub_components[1]))
+    user_data = result.one_or_none()
+
+    return UserPublic.model_validate(user_data)
